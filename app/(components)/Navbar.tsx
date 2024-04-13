332bc16b869a4e0b652041bf893078be3a53e48c
Ashcode <akshaycodeur@gmail.com>
2024-04-13 10:51:59 -0400

    style: navbar links using classnames :lipstick:

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,8 +1,13 @@
+"use client";
 import Link from "@/node_modules/next/link";
+import { usePathname } from "@/node_modules/next/navigation";
 import React from "react";
 import { AiFillBug } from "react-icons/ai";
+import classnames from "classnames";
 
 const Navbar = () => {
+	let currentPath = usePathname();
+	console.log(currentPath);
 	const links = [
 		{ label: "Dashboard", href: "/" },
 		{ label: "Issues", href: "/issues" },
@@ -16,7 +21,11 @@ const Navbar = () => {
 				{links.map((link) => (
 					<Link
 						key={link.href}
-						className="text-zinc-500 hover:text-zinc-800 transition-colors"
+						className={classnames({
+							"text-zinc-900": link.href === currentPath,
+							"text-zinc-500": link.href !== currentPath,
+							"hover:text-zinc-800 transition-colors": true,
+						})}
 						href={link.href}
 					>
 						{link.label}
