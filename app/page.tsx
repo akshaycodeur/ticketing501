c1e8fdaf0f41b7d1cda742dd32057bed27c62bed
Ashcode <akshaycodeur@gmail.com>
2024-04-13 10:25:14 -0400

    chore: added Navbar :construction:

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-	return (
-		<div className="">
-			<h1 className="text-3xl">This ain't Sparta</h1>
-		</div>
-	);
+	return <div className=""></div>;
 }
