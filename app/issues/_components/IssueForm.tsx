97e1a8b8e11bf86f4a9330d9060944c6be50b6f5
Ashcode <akshaycodeur@gmail.com>
2024-04-14 10:36:41 -0400

    chore: build api to update issue

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -9,13 +9,13 @@ import React from "react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { createIssueSchema } from "@/app/validationSchemas";
+import { issueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/(components)/ErrorMessage";
 import Spinner from "@/app/(components)/Spinner";
 import dynamic from "next/dynamic";
 
-type IssueFormData = z.infer<typeof createIssueSchema>;
+type IssueFormData = z.infer<typeof issueSchema>;
 
 const IssueForm = ({ issue }: { issue?: Issue }) => {
 	const router = useRouter();
@@ -25,7 +25,7 @@ const IssueForm = ({ issue }: { issue?: Issue }) => {
 		handleSubmit,
 		formState: { errors },
 	} = useForm<IssueFormData>({
-		resolver: zodResolver(createIssueSchema),
+		resolver: zodResolver(issueSchema),
 	});
 	const [error, setError] = useState("");
 	const [isSubmitting, setSubmitting] = useState(false);
