666ec62b911dcd26194d53514320941738979302
Ashcode <akshaycodeur@gmail.com>
2024-04-13 20:37:48 -0400

    chore: error component made;

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,6 +11,7 @@ import { useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
+import ErrorMessage from "@/app/(components)/ErrorMessage";
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
@@ -51,11 +52,7 @@ const NewIssuePage = () => {
 					})}
 				>
 					<TextField.Root placeholder="Title" {...register("title")} />
-					{errors.title && (
-						<Text color="red" as="p">
-							{errors.title.message}
-						</Text>
-					)}
+					<ErrorMessage>{errors.title?.message}</ErrorMessage>
 					<Controller
 						name="description"
 						control={control}
@@ -63,11 +60,7 @@ const NewIssuePage = () => {
 							<SimpleMDE placeholder="Description" {...field} />
 						)}
 					/>
-					{errors.description && (
-						<Text color="red" as="p">
-							{errors.description.message}
-						</Text>
-					)}
+					<ErrorMessage>{errors.description?.message}</ErrorMessage>
 					<div className="flex justify-end">
 						<Button>Submit New Issue</Button>
 					</div>
