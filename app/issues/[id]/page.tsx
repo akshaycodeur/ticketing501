b0e40f4296b5b449d0e5b3ed61457df3879fceb9
Ashcode <akshaycodeur@gmail.com>
2024-04-14 09:39:47 -0400

    chore: SRP application making edit issue details and button in seperate component

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -6,6 +6,8 @@ import React from "react";
 import ReactMarkdown from "react-markdown";
 import Link from "@/app/(components)/Link";
 import { Pencil2Icon } from "@radix-ui/react-icons";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
 	params: { id: string };
@@ -23,25 +25,13 @@ const IssueDetailPage = async ({ params }: Props) => {
 			<div className="flex-row md:flex  gap-10">
 				<div className="w-full md:w-3/5 mx-auto bg-zinc-50 px-10 py-5 rounded-sm shadow-md">
 					<Box>
-						<Heading>{issue.title}</Heading>
-						<Flex className="space-x-3" my="2">
-							<IssueStatusBadge status={issue.status} />
-							<Text>{issue.createdat.toDateString()}</Text>
-						</Flex>
-						<Card className="prose" mt="4">
-							<ReactMarkdown>{issue.description}</ReactMarkdown>
-						</Card>
+						<IssueDetails issue={issue} />
 					</Box>
 				</div>
 
 				<div className="w-full md:w-2/5 mx-auto">
 					<Box>
-						<Link href={`/issues/${issue.id}/edit`}>
-							<Button>
-								<Pencil2Icon />
-								Edit Issue
-							</Button>
-						</Link>
+						<EditIssueButton issueId={issue.id} />
 					</Box>
 				</div>
 			</div>
