5d03b8737d8a9c1739bb38d709224c7ae093b7d0
Ashcode <akshaycodeur@gmail.com>
2024-04-14 07:07:41 -0400

    chore. handle submit made into component

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -29,6 +29,17 @@ const NewIssuePage = () => {
 	const [error, setError] = useState("");
 	const [isSubmitting, setSubmitting] = useState(false);
 
+	const onSubmit = handleSubmit(async (data) => {
+		try {
+			setSubmitting(true);
+			await axios.post("/api/issues", data);
+			router.push("/issues");
+		} catch (error) {
+			setSubmitting(false);
+			setError("An unexpected error occurred.");
+		}
+	});
+
 	return (
 		<div className="max-w-xl mx-auto bg-zinc-50 px-10 py-5 rounded-sm shadow-md">
 			<div className="max-w-xl ">
@@ -42,19 +53,7 @@ const NewIssuePage = () => {
 					</Callout.Root>
 				)}
 
-				<form
-					className="space-y-3"
-					onSubmit={handleSubmit(async (data) => {
-						try {
-							setSubmitting(true);
-							await axios.post("/api/issues", data);
-							router.push("/issues");
-						} catch (error) {
-							setSubmitting(false);
-							setError("An unexpected error occurred.");
-						}
-					})}
-				>
+				<form className="space-y-3" onSubmit={onSubmit}>
 					<TextField.Root placeholder="Title" {...register("title")} />
 					<ErrorMessage>{errors.title?.message}</ErrorMessage>
 					<Controller
